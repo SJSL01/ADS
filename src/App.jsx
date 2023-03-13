fd35f70e8b6e4cbcf406ef58bacd62237ccd0aed
Simran Jyot SIngh Lall <120094702+SJSL01@users.noreply.github.com>
2023-03-13 23:24:55 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <div style={{
+      height: "100vh", display: "flex",
+      flexDirection: "column", justifyContent:"center", alignItems:"center"
+    }}>
+      <h1>DUMMY</h1>
+    </div>
+  )
+}
+
+export default App
