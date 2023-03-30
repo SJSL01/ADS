94250df0afb08d0526ba42f578266cfb1cbaa74b
Simran Jyot SIngh Lall <120094702+SJSL01@users.noreply.github.com>
2023-03-31 00:42:56 +0530

    Files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
-
+import "./style.css"
 
 function App() {
 
 
+
+
   return (
-    <div style={{
-      height: "100vh", display: "flex",
-      flexDirection: "column", justifyContent:"center", alignItems:"center"
-    }}>
-      <h1>DUMMY</h1>
-    </div>
+
+    <>
+
+
+      <header>
+        
+        <nav>
+          <h1>DUMMY</h1>
+          <ul>
+            <li>HOME</li>
+            <li>PRODUCTS</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+
+      </header>
+
+
+      <main>
+
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+        <div>
+          content
+        </div>
+
+      </main>
+
+
+      <footer>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+        <div>dummy</div>
+      </footer>
+
+
+
+    </>
   )
 }
 
